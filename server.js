09ab8ad72087d85f12749cd7f7c41fe3ec57cd62
Beduman <behunz77@gmail.com>
2024-09-26 18:18:19 -0700

    .

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongodb = require("./data/database.js");
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 3000;
  
 app.get('/', (req, res) => {
   res.send("Hello");
